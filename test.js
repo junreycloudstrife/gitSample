cfdec45cf7cc0f41513289055c30111572080b05
junreycloudstrife <junrey.cloudstrife@gmail.com>
2018-03-21 23:41:06 +0800

    Updated test.js

--- test.js
+++ test.js
@@ -1,1 +1,30 @@
-console.log('This is a test.js file')
+//https://www.npmjs.com/package/keypress
+
+var keypress = require('keypress');
+var text = '';
+ 
+// make `process.stdin` begin emitting "keypress" events 
+keypress(process.stdin);
+ 
+// listen for the "keypress" event 
+process.stdin.on('keypress', function (ch, key) {
+  // console.log('got "keypress"', key);
+  //console.log(key.name);
+  // text = text + key.name;
+  // console.log(text);
+
+  if (key.name == 'return') {
+    console.log("\n" + text);
+    text = '';
+  } else {
+  	process.stdout.write(key.name);
+  	text = text + key.name;
+  }
+
+  if (key && key.ctrl && key.name == 'c') {
+    process.stdin.pause();
+  }
+});
+ 
+process.stdin.setRawMode(true);
+process.stdin.resume();
