59499ed4b8921746a66e2876ccdcee78aed87bc8
junreycloudstrife <junrey.cloudstrife@gmail.com>
2018-03-22 00:03:09 +0800

    Put comments on test.js.

--- test.js
+++ test.js
@@ -13,17 +13,26 @@ process.stdin.on('keypress', function (ch, key) {
   // text = text + key.name;
   // console.log(text);
 
-  if (key.name == 'return') {
-    console.log("\n" + text);
-    text = '';
-  } else {
-  	process.stdout.write(key.name);
-  	text = text + key.name;
-  }
+	// This part will detect if Enter key is pressed
+	// if true then will break to next line and
+	// diplay the content of text variable and
+	// then will clear the content of text variable
+	if (key.name == 'return') {
+		console.log("\n" + text);
+		text = '';
+	// If the Enter key is not pressed then it just
+	// display the character being typed while 
+	// storing the characters to text variable
+	} else {
+		process.stdout.write(key.name);
+ 		text = text + key.name;
+	}
 
-  if (key && key.ctrl && key.name == 'c') {
-    process.stdin.pause();
-  }
+	// This part will detect if 'Ctrl' + 'C' is being
+	// pressed. If true then will exit the program. 
+	if (key && key.ctrl && key.name == 'c') {
+		process.stdin.pause();
+	}
 });
  
 process.stdin.setRawMode(true);
