0f07e73211a469ba7fb551e174249645ccc5fed1
junreycloudstrife <junrey.cloudstrife@gmail.com>
2018-03-22 19:33:12 +0800

    Fixed bugs on entering same input

--- common_prefix.js
+++ common_prefix.js
@@ -1,39 +1,6 @@
 // Give two words and find the common prefix
 var ask = require('./getInput');
 
-// // version 1
-// ask.getInput("First Word: ", function(firstWord){
-// 	ask.getInput("Second Word: ", function(secondWord){
-// 		// console.log(firstWord + secondWord);
-// 		var i = 0;
-// 		var temp = "";
-
-// 		if (firstWord.length < secondWord.length) {
-// 			while (i <= firstWord.length) {
-// 				temp = firstWord.charAt(i);
-// 				if (temp == secondWord.charAt(i)) {
-// 					i++;					
-// 				} else {
-// 					console.log("The long prefix is " + firstWord.slice(0,i));
-// 					break;
-// 				}
-// 			}
-// 		} else {
-// 			while (i <= secondWord.length){
-// 				temp = secondWord.charAt(i);
-// 				if (temp == firstWord.charAt(i)) {
-// 					i++;					
-// 				} else {
-// 					console.log("The long prefix is " + secondWord.slice(0,i));
-// 					break;
-// 				}
-// 			}
-// 		}
-// 		process.exit();
-// 	})
-// })
-
-// version 2
 ask.getInput("Enter the first word : ", function(firstWord){
 	ask.getInput("Enter the second word: ", function(secondWord){
 		var index = 0;
@@ -43,21 +10,24 @@ ask.getInput("Enter the first word : ", function(firstWord){
 			temp = firstWord;
 			firstWord = secondWord;
 			secondWord = temp;
-		}
+		};
 
 		if (firstWord.length <= secondWord.length) {
 			while (index <= firstWord.length) {
 				temp = firstWord.charAt(index);
 				if (temp == secondWord.charAt(index)) {
-					index++;					
+					if (index < firstWord.length) {
+						index++;					
+					} else {
+						console.log("The common prefix is " + firstWord.slice(0,index));
+						break;
+					};
 				} else {
 					console.log("The common prefix is " + firstWord.slice(0,index));
 					break;
-				}
-			}
-		} 
+				};
+			};
+		}; 
 		process.exit();
-	})
-})
-
-
+	});
+});
