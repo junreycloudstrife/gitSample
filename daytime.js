e9e67224fdbd29c401f053eeacc0dfdde965d9ac
junreycloudstrife <junrey.cloudstrife@gmail.com>
2018-03-29 16:12:27 +0800

    Initial commit.

---
+++ daytime.js
@@ -0,0 +1,18 @@
+// Source: https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-1.php
+// console.log("Write a JavaScript program to display the current day and time in the following format.");
+// console.log("Today is : Friday."); 
+// console.log("Current time is : 4 PM : 50 : 22") 
+
+
+var date = new Date();
+var days = ["Sunday","Monday","Tuesday","Friday","Saturday"];
+console.log("Today is : " + days[date.getDay()] + ".");
+console.log("Current time is : " + hour() + " : " + date.getMinutes() + " : " + date.getSeconds());
+
+function hour(){
+	if (date.getHours()>=12){
+		return date.getHours()-12 + " PM"
+	} else {
+		return date.getHours() + " AM"
+	}
+}
